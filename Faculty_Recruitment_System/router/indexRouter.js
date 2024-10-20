ce0194ede22fe830eae4aba60b004ba3eed332f4
AryanSengar01 <113140044+AryanSengar01@users.noreply.github.com>
2024-10-20 14:10:06 +0530

    Add files via upload

--- Faculty_Recruitment_System/router/indexRouter.js
+++ Faculty_Recruitment_System/router/indexRouter.js
@@ -5,5 +5,15 @@ indexRouter.get("/",(request,response)=>{
     response.render("index");
 });
 
+indexRouter.get("/services",(request,response)=>{
+    response.render("services");
+})
 
+indexRouter.get("/contact",(request,response)=>{
+    response.render("contact");
+})
+
+indexRouter.get("/about",(request,response)=>{
+    response.render("about");
+})
 export default indexRouter;
